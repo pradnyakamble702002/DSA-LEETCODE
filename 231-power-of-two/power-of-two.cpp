a7baf9e5c4cd3cc57023a9d324d7e39cea11c15f
pradnyakamble702002 <pradnyakamble7020@gmail.com>
2026-07-20 21:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (12.26%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int i;
+        for(i=0;i<=30;i++)
+        {
+       int ans=pow(2,i);
+       if(ans==n)
+       {
+        return true;
+       }
+        
+
+        }
+       return false;
+        } 
+    
+};
